9dbd52a06ac7359626cb6c8466bf598d2a566bd8
Diego Rios <diegoarv08@hotmail.com>
2025-02-20 23:38:57 -0400

    Segundo Paso al hacer TDD

---
+++ src/fizzbuzz.js
@@ -0,0 +1,7 @@
+function fizzBuzz(n){
+
+    let resultado = "1";
+
+    return resultado;
+}
+export default fizzBuzz;
--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,6 +1,8 @@
 
 describe("FizzBuzz", () => {
+  
   it("Deberia generar el mismo numero si el numero no sigue una regla", () => {
     expect(fizzbuzz(1)).toEqual("1");
   });
+  
 });
