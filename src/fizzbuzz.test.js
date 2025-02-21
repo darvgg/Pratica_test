0563197097aaf3e69bd475ad08806be444ed97ee
Diego Rios <diegoarv08@hotmail.com>
2025-02-20 23:50:27 -0400

    Errores de escritura corregidos

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,20 +1,21 @@
 import fizzBuzz from "./fizzbuzz";
+
 describe("FizzBuzz", () => {
   
   it("Deberia generar el mismo numero si el numero no sigue una regla", () => {
-    expect(fizzbuzz(1)).toEqual("1");
+    expect(fizzBuzz(1)).toEqual("1");
   });
 
   it("Deberia generar el primer fizz", () => {
-    expect(fizzbuzz(3)).toEqual("fizz");
+    expect(fizzBuzz(3)).toEqual("fizz");
   });
 
   it("Deberia generar el primer buzz", () => {
-    expect(fizzbuzz(3)).toEqual("buzz");
+    expect(fizzBuzz(5)).toEqual("buzz");
   });
 
   it("Deberia generar el primer fizzbuzz", () => {
-    expect(fizzbuzz(3)).toEqual("fizzbuzz");
+    expect(fizzBuzz(15)).toEqual("fizzbuzz");
   });
 
 });
